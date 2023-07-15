fd10c8a9e453e1fa9ea2001a3b04609b47b11090
buvaneshp <buvaneshwaranp@spritle.com>
2023-07-15 18:21:05 +0530

    training_event and category model

---
+++ db/migrate/20230715102754_create_training_events.rb
@@ -0,0 +1,16 @@
+class CreateTrainingEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :training_events do |t|
+      t.string :title, null: false
+      t.references :category, null: false, foreign_key: true
+      t.integer :status
+      t.json :data
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+    add_index :training_events, :status
+    add_index :training_events, :title
+    add_index :training_events, %i[status category_id], name: 'index_training_events_on_status_category_id'
+    add_index :training_events, %i[status user_id], name: 'index_training_events_on_status_user_id'
+  end
+end
