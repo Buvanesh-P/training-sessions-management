0b9035f791fa6a46a439a344c1a0c2eb09e28f7d
buvaneshp <buvaneshwaranp@spritle.com>
2023-07-15 17:29:34 +0530

    added columns for user table

---
+++ db/migrate/20230715093310_add_column_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :designation, :string
+    add_column :users, :bio, :text
+
+    add_index :users, :name
+  end
+end
